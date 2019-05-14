c171410ffb4443b109593cdf96ecd7f0776cbc6f
Jonas Havers <jh@jonas-havers.de>
2019-05-14 21:32:26 +0200

    Add another domain test

--- src/test/java/com/company/app/architecture/DomainComponentsTest.java
+++ src/test/java/com/company/app/architecture/DomainComponentsTest.java
@@ -7,6 +7,14 @@ import org.junit.Test;
 public class DomainComponentsTest extends ArchitectureTest {
 
   @Test
+  public void domainClassesShouldOnlyBeAccessedByOtherDomainClassesOrTheApplicationLayer() {
+    ArchRule rule = ArchRuleDefinition.classes()
+      .that().resideInAPackage(DOMAIN_LAYER_PACKAGES)
+      .should().onlyBeAccessed().byAnyPackage(DOMAIN_LAYER_PACKAGES, APPLICATION_LAYER_PACKAGES);
+    rule.check(classes);
+  }
+
+  @Test
   public void domainClassesShouldOnlyDependOnDomainOrStdLibClasses() {
     ArchRule rule = ArchRuleDefinition.classes()
       .that().resideInAPackage(DOMAIN_LAYER_PACKAGES)
